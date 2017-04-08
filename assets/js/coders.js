ef490d046e5129ba7cfd87792557b3f9cebbde54
user <mary20bw@gmail.com>
2017-04-07 23:32:20 -0500

    refactorizando

--- assets/js/coders.js
+++ assets/js/coders.js
@@ -4,9 +4,9 @@ document.getElementById("toggle").addEventListener("click", function(event){
   document.getElementById('body').classList.toggle("overflow-hidden");
 });
 
-var lastScrollTop = 0;
+ var lastScrollTop = 0;
 window.addEventListener("scroll", function(){
-   var currentScroll = window.pageYOffset || document.documentElement.scrollTop;
+ var currentScroll = window.pageYOffset || document.documentElement.scrollTop;
    if (currentScroll > lastScrollTop){
      document.getElementById('nav-background').style.display="none";
      document.getElementById("nav-header").classList.remove("header-in");
@@ -24,27 +24,34 @@ window.addEventListener("scroll", function(){
    lastScrollTop = currentScroll;
  }, false);
 
+/* var coders=document.getElementsByClassName("coder6Lima");*/
+  var contenedorMayor= document.getElementById("ContenedorCoders");
+  var coders = ['','Fiorella Quispe','Lourdes Vilchez','Leydi Maldonado','Yessenia Huamán',
+               'Miriam Mendoza', 'Elizabeth Condori', 'Arantza Burga', 'Grecia Rayme',
+               'Janine Vega', 'Rosario Félix', 'Daguiana Revolledo', 'Jenny Velasquez',
+               'Nadia Cuadros', 'Michelle More', 'Marilu Llamoca', 'Mariel García',
+               'Fiorella Cisneros', 'Geraldine Chauca', 'Yelitza Choque',
+               'Stephanie Hiyagon', 'Rocio Tapia', 'Dana Franco', 'Flor Retamozo',
+               'Nathaly Pacheco', 'Ericka Vidal', 'Katherine Ortega', 'Brilly Majuan',
+               'Flor de Cantuta', 'Leslie Avendaño', 'Cindy Mendoza', 'Annia Flores',
+              'Betsi Loayza', 'Ayda Sulca', 'Milagros Gutierrez', 'Nakarid Jave',
+              'Angie Condor', 'Maricarmen Rojas', 'Ariana Cabana', 'Flor Tello',
+              'Mitchell Rodríguez', 'Naomi Villanueva', 'Mary Castillo', 'Miriam Peralta',
+              'Karin Alejo', 'Liliana Peña', 'Ruth Salvador', 'Marilu Llamoca', 'Wendy Reyes',
+              'Cinthya Quispe','Maria Grecia Cutipa', 'Ana Durand', 'Glisse Jorge', 'Neiza Nuñez',
+              'Sandra Solorzano'];
+
+    for (var i = 1; i<=54; i++) {
+    var foto=document.createElement('img');
+    var divCoders=document.createElement('div');
+    var span=document.createElement('span');
+    divCoders.setAttribute('class','div-figura');
+    foto.setAttribute('src','assets/img/students/'+ i +'.png');
+
+    span.innerHTML=coders[i];
 
 
-  /* var coders=document.getElementsByClassName("coder6Lima");*/
-  var coders=[arantza-burga,cindy-mendoza,cinthia,daguiana-revoredo,Emma-tapia,elizabeth,
-  fiorella-cisneros,fio-quispe,flor-tello,FlorRetamozo,geraldine-chauca,glisse-jorge,grecia-rayme,
-  janine-vega,karin-Alejo,liliana-Peña,LourdesVilchez,maria-grecia,natha-astolar,mary-castillo,marilu-llamocca,
-rosario-felix,neiza-nunez,mishel-velasquez,nadia-cuadros,michelle-more,miriam,miriam-peralta,stephanie-hiyagon,nombre];
-
-   function creandoDiv(){
-		var divMayor= document.getElementById("ContenedorCoders")
-		divMayor.innerHTML="";
-
-    for(var i = 0; i<coders.length;i++){
-			var divCoders = document.createElement("div");
-			divCoders.classList.add("coder");/*clase coder*/
-
-			for (var prop in coders[i]) {
-				var dato = document.createElement("p");
-				dato.innerHTML= prop + " : " + coders[i][prop];
-				divCoders.appendChild(dato);
-			}
-			divMayor.appendChild(divCoders);
-		}
+    divCoders.appendChild(foto);
+    divCoders.appendChild(span);
+    contenedorMayor.appendChild(divCoders);
   }
